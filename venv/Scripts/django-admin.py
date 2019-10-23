1c28088e636d6607be0a810392f16d1a8999591a
ArturoMonterroso2598 <arturomonterroso3@gmail.com>
2019-10-22 22:29:36 -0600

    Intengo 2

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\josea\desktop\8vosem~1\progra~1\ejerci~3\crud-a~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
